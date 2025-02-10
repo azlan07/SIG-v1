b2b548eb9521f11cbfc535be66913ed1843458b4
Lanch-UTie <azlan.nur15.07hakim@gmail.com>
2025-02-10 21:16:47 +0700

    first commit

---
+++ admin/sample.php
@@ -0,0 +1,66 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+
+    <!-- Bootstrap CSS -->
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"> -->
+    <!-- Font Awesome untuk icons -->
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"> -->
+
+    <!-- Custom Styling -->
+    <link rel="shortcut icon" type="image/png" href="./assets/images/logos/favicon.png" />
+    <link rel="stylesheet" href="./assets/css/styles.min.css" />
+</head>
+
+<body>
+    <!--  Body Wrapper -->
+    <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+        data-sidebar-position="fixed" data-header-position="fixed">
+
+        <!-- Include Sidebar -->
+        <?php include 'components/sidebar.php'; ?>
+
+        <!--  Main wrapper -->
+        <div class="body-wrapper">
+
+            <!-- Include Navbar -->
+            <?php include 'components/navbar.php'; ?>
+
+            <div class="container-fluid">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title fw-semibold mb-4">Sample Page</h5>
+                        <p class="mb-0">This is a sample page </p>
+                    </div>
+                </div>
+
+                <!-- Include Footer -->
+                <?php include 'components/footer.php'; ?>
+
+            </div>
+        </div>
+    </div>
+
+    <!-- Custom Script -->
+    <script src="./assets/libs/jquery/dist/jquery.min.js"></script>
+    <script src="./assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="./assets/js/sidebarmenu.js"></script>
+    <script src="./assets/js/app.min.js"></script>
+    <script src="./assets/libs/apexcharts/dist/apexcharts.min.js"></script>
+    <script src="./assets/libs/simplebar/dist/simplebar.js"></script>
+    <script src="./assets/js/dashboard.js"></script>
+
+    <!-- Bootstrap JS and dependencies -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script> -->
+    <!-- Add jQuery -->
+    <!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> -->
+</body>
+
+</html>
